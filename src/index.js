f9df7ee91aafe7ed80e0cea4624d6c9f61e976f1
Kasper1999 <sasha.kasper12@mail.ru>
2018-09-30 10:09:23 +0300

    first try

--- src/index.js
+++ src/index.js
@@ -1,3 +1,85 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var symbols = str.split('');
+  var openBracketsStack = [];
+  var result = true;
+  debugger;
+  
+  for (i = 0; i < symbols.length; i++) {
+      var curSym  = symbols[i];
+      if (curSym === '(' || curSym === '[' || curSym === '{' || curSym === '1' || curSym === '3' || curSym === '5' || curSym === '7' || curSym === '8') {
+          openBracketsStack.push(curSym);
+      } else if (curSym === ')' || curSym === ']' || curSym === '}' || curSym === '2' || curSym === '4' || curSym === '6' || curSym === '7' || curSym === '8') {
+          if (openBracketsStack.length > 0) {
+              if (curSym === ')') {
+                  var lastOpenBracket = openBracketsStack[openBracketsStack.length - 1];
+                  if (lastOpenBracket === '(') {
+                      openBracketsStack.pop();
+                  } else {
+                      result = false;
+                  }
+              } else if (curSym === ']') { 
+                  var lastOpenBracket = openBracketsStack[openBracketsStack.length - 1];
+                  if (lastOpenBracket === '[') {
+                      openBracketsStack.pop();
+                  } else {
+                      result = false;
+                  }
+              } else if (curSym === '}') {
+                  var lastOpenBracket = openBracketsStack[openBracketsStack.length - 1];
+                  if (lastOpenBracket === '{') {
+                      openBracketsStack.pop();
+                  } else {
+                      result = false;
+                  }
+} else if (curSym === '2') { 
+                  var lastOpenBracket = openBracketsStack[openBracketsStack.length - 1];
+                  if (lastOpenBracket === '1') {
+                      openBracketsStack.pop();
+                  } else {
+                      result = false;
+                  }
+              } else if (curSym === '4') {
+                  var lastOpenBracket = openBracketsStack[openBracketsStack.length - 1];
+                  if (lastOpenBracket === '3') {
+                      openBracketsStack.pop();
+                  } else {
+                      result = false;
+                  }
+              } else if (curSym === '6') {
+                  var lastOpenBracket = openBracketsStack[openBracketsStack.length - 1];
+                  if (lastOpenBracket === '5') {
+                      openBracketsStack.pop();
+                  } else {
+                      result = false;
+                  }
+              } else if (curSym === '7') {
+                  var lastOpenBracket = openBracketsStack[openBracketsStack.length - 1];
+                  if (lastOpenBracket === '7') {
+                      openBracketsStack.pop();
+                  } else {
+                      result = false;
+                  }
+              } else if (curSym === '8') {
+                  var lastOpenBracket = openBracketsStack[openBracketsStack.length - 1];
+                  if (lastOpenBracket === '8') {
+                      openBracketsStack.pop();
+                  } else {
+                      result = false;
+                  }
+
+              }
+          } else {
+              result = false;
+          }
+      }
+  }
+  
+  if (openBracketsStack.length > 0) {
+      result = false;
+  }
+
+
+  return(result);
+
 }
